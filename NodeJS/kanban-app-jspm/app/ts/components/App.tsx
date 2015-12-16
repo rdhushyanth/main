b3f00299ff7a342c6468b695ad4c3b2f0e9cee76
Dhushyanth Ramasamy <r.dushyanth@gmail.com>
2015-12-16 01:23:29 -0500

    SurviveJS example using jspm , Typescript
    This is an example usage of the SurviveJs tutorial using jspm instead
    of webpack.

---
+++ NodeJS/kanban-app-jspm/app/ts/components/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Note from './Note';
+
+export default () => {
+  return  <Note />;
+};
---
+++ NodeJS/kanban-app-jspm/app/ts/components/Note.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export default class Note extends React.Component <{}, {}>{
+  render() {
+    return  <div>Learn  Webpacks</div>;
+  }
+}
